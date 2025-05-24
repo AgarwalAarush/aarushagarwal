f84c4f0c8bd8b5a42cb95a0463118b66ad18593a
Aarush Agarwal <aarushaga@gmail.com>
2025-05-23 20:33:03 -0400

    fixed font loading, made local

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,10 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preload" href="/fonts/ApothemCapsMD.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="/fonts/JetBrains Mono/JetBrainsMono-Regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="/fonts/JetBrains Mono/JetBrainsMono-Medium.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="/fonts/JetBrains Mono/JetBrainsMono-SemiBold.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="/fonts/JetBrains Mono/JetBrainsMono-Bold.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
       </Head>
       <body className="antialiased">
         <Main />
